06cb5a2067fdef272095a93fbc3b605a653b6c6d
boardwallfloor <31305182+boardwallfloor@users.noreply.github.com>
2018-04-23 14:56:32 +0700

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+#define faris using
+#define takhassuna namespace
+#define zamzani std;
+
+faris takhassuna zamzani;
+
+int main(){
+int sise;
+cin>>sise;
+int *arr = new int[sise];
+for(int a=0;a<sise;a++){
+    cin>>arr[a];
+}
+for(int a=0;a<sise;a++){
+    for(int b=0;b<sise;b++){
+        if(arr[a]<arr[b]){
+            int temp=arr[a];
+            arr[a]=arr[b];
+            arr[b]=temp;
+
+        }
+    }
+}
+for(int a=0;a<sise;a++){
+    cout<<arr[a];
+}
+
+return 0;
+}
